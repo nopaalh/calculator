918697dbf20a63845430b5e7825d0b8279e94479
nopalh <nopalh25@gmail.com>
2026-07-13 14:34:59 +0700

    make the logic for upper screen, clear, all clear, and operating calculation(from this only sum logic made bcs i gtg)

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-let screenLower = [];
+let firstNumber = null;
+let operator = null;
 let shouldReset = false;
 
 const lowerScreenNumber = document.querySelector(".screen .lower");
+const upperScreenNumber = document.querySelector(".screen .upper");
 const buttons = document.querySelector("button");
 const keypad = document.querySelector(".keypad");
 
@@ -11,10 +13,20 @@ keypad.addEventListener("click", (e) => {
     appendNumber(e.target.textContent);
   } else if (e.target.classList.contains("btnLight")) {
     if (e.target.textContent === "AC") {
-      resetInput();
+      allClear();
     } else if (e.target.textContent === "C") {
-      resetInput();
+      clear();
     }
+  } else if (e.target.classList.contains("btnOrange")) {
+    if (e.target.textContent === "=") {
+      if (operator !== null) {
+        operate(firstNumber, operator, Number(lowerScreenNumber.textContent));
+      }
+      return;
+    }
+    operator = e.target.textContent;
+    firstNumber = Number(lowerScreenNumber.textContent);
+    shouldReset = true;
   }
 });
 
@@ -22,15 +34,30 @@ const appendNumber = (number) => {
   if (lowerScreenNumber.textContent.length === 16) {
     return;
   }
-  if (lowerScreenNumber.textContent == 0) {
+  if (lowerScreenNumber.textContent == 0 || shouldReset) {
+    if (shouldReset) {
+      upperScreenNumber.textContent =
+        lowerScreenNumber.textContent + " " + operator;
+      shouldReset = false;
+    }
     lowerScreenNumber.textContent = "";
   }
   lowerScreenNumber.textContent += number;
   changeFontSize(lowerScreenNumber.textContent.length);
 };
 
-const resetInput = () => {
+const allClear = () => {
   lowerScreenNumber.textContent = "0";
+  upperScreenNumber.textContent = "";
+
+  firstNumber = null;
+  operator = null;
+  shouldReset = false;
+};
+
+const clear = () => {
+  lowerScreenNumber.textContent = "0";
+  shouldReset = false;
 };
 
 const changeFontSize = (len) => {
@@ -42,5 +69,27 @@ const changeFontSize = (len) => {
     lowerScreenNumber.style.fontSize = "50px";
   } else if (len === 12) {
     lowerScreenNumber.style.fontSize = "42px";
+  }
+};
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const operate = (firstNumber, operator, secondNumber) => {
+  switch (operator) {
+    case "+":
+      let ans = sum(firstNumber, secondNumber);
+      lowerScreenNumber.textContent = ans;
+      upperScreenNumber.textContent = "";
+      break;
+    case "-":
+      break;
+    case "/":
+      break;
+    case "*":
+      break;
+    default:
+      break;
   }
 };
